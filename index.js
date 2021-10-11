516b6a0a126acfcb60e661bed69d1bc126c6bb97
Francois Janson <jansonfrancois@gmail.com>
2021-10-11 18:19:06 +0200

    cleaning code

--- index.js
+++ index.js
@@ -1,39 +1,65 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose");
-const StudentModel = require("./models/StudentModel");
-const AddressModel = require("./models/AddressModel");
+const Student = require("./models/Student");
+const Address = require("./models/Address")
 const db = "mongodb://localhost:27017/populate"
 const port = 8000;
 
-const address = new Address({
+const address1 = new Address({
     streetName: "rue des champs",
     streetNumber: "14",
     postCode: "75001",
     city: "Paris"
-})
+});
+// address1.save((err) => {
+//     if (err) {
+//         console.error(err)
+//     }
+//     const addressId = address1._id
+//     const student = new Student({
+//         firstName: "Francois",
+//         surName: "Janson",
+//         address: addressId
+//     })
+//     student.save((err) => {
+//         if (err) {
+//             console.error(err)
+//         }
+//     })
+// })
 
-address.save((err) => {
-    if (err) {
-        console.error(err)
-    }
-    const addressId = address._id
-    const student = new Student({
-        firstName: "Francois",
-        surName: "Janson",
-        address: addressId
-    })
-    student.save((err) => {
-        if (err) {
-            console.error(err)
-        }
-    })
-})
 
 Student.find()
     .populate("address")
     .then(student => console.log(student))
     .catch(err => console.error(err))
+
+const saveData = (student, address) => {
+    const addressToSave = new Address(address)
+    addressToSave.save((err) => {
+        if (err) {
+            console.error(err)
+        }
+        student.address = addressToSave._id
+        const studentToSave = new Student(student)
+        studentToSave.save(err => {
+            if (err) {
+                console.error(err)
+            }
+        })
+    })
+}
+
+saveData({
+    firstName: "John",
+    surName: "Doe",
+}, {
+    streetName: "Thing Street",
+    streetNumber: "14",
+    postCode: "25145",
+    city: "NeverLand"
+})
 
 mongoose.connect(db, (error) => {
     if (error) {
